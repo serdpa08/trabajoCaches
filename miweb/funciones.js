5891b9858ee0f752e6de3f533bc06eca13d6455d
sergio de pablos <sergioleng18@gmail.com>
2020-01-08 18:10:40 +0100

    parte de webStorage

--- miweb/funciones.js
+++ miweb/funciones.js
@@ -1,7 +1,34 @@
 document.addEventListener('DOMContentLoaded', inicio);
 
 function inicio(){
-    console.log(window.applicationCache.status);
+    
+        //console.log("Estado de la cache: "+window.applicationCache.status);
+    
+    
+
+    switch (window.applicationCache.status) {
+        case 0: // UNCACHED == 0
+        console.log("Estado de la cache: "+window.applicationCache.status);
+          break;
+        case 1: // IDLE == 1
+        console.log("Estado de la cache: "+window.applicationCache.status);
+          break;
+        case 2: // CHECKING == 2
+        console.log("Estado de la cache: "+window.applicationCache.status);
+          break;
+        case 3: // DOWNLOADING == 3
+        console.log("Estado de la cache: "+window.applicationCache.status);
+          break;
+        case 4:  // UPDATEREADY == 4
+        console.log("Estado de la cache: "+window.applicationCache.status);
+          break;
+        case 5: // OBSOLETE == 5
+          console.log("Estado de la cache: "+window.applicationCache.status);
+          break;
+        default:
+          console.log( 'UKNOWN CACHE STATUS');
+          break;
+      };
 
 }
 
